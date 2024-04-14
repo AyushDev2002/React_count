79a7100da2c93b0603c281e096ec4d16843bd11f
Ayush <tomar54490@gmail.com>
2024-04-14 21:01:05 +0530

    Extra code removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import Navbar from './components/Navbar';
 
 export default function App() {
   const [count, setcount] = React.useState(0)
-  let number = 0
-  function setNumber(value){
-    number = value
-  }
   function increasevalue(num){
     if (count < 20){
       setcount(count + num) 
